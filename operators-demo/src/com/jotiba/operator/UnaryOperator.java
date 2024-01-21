a0a8f9ad24fe978d852cc749168a6f97b8190918
jotiba <jotiba.marathe1@gmail.com>
2024-01-21 10:59:49 +0530

    unary operator added

--- operators-demo/src/com/jotiba/operator/UnaryOperator.java
+++ operators-demo/src/com/jotiba/operator/UnaryOperator.java
@@ -1,52 +1,54 @@
 package com.jotiba.operator;
+
 import java.util.Scanner;
 
 public class UnaryOperator {
     int input = 0;
 
-    public void postIncrement(int input){
+    public static void main(String[] args) {
+
+        System.out.println("Enter integer value");
+        Scanner sc = new Scanner(System.in);
+        String inputNumber = sc.nextLine();
+
+        UnaryOperator UnaryOperator = new UnaryOperator();
+        int input = Integer.valueOf(inputNumber);
+
+        UnaryOperator.postIncrement(input);
+        UnaryOperator.preIncrement(input);
+        UnaryOperator.postDecrement(input);
+        UnaryOperator.preDecrement(input);
+        UnaryOperator.complementRepresentation(input);
+        UnaryOperator.negationOperator(true);
+
+    }
+
+    public void postIncrement(int input) {
         System.out.println("Post increment: " + input++);
         System.out.println("After post increment: " + input);
     }
 
-    public void preIncrement(int input){
+    public void preIncrement(int input) {
         System.out.println("Pre increment: " + ++input);
         System.out.println("After pre increment: " + input);
     }
 
-    public void postDecrement(int input){
+    public void postDecrement(int input) {
         System.out.println("Post decrement: " + input--);
         System.out.println("After post decrement: " + input);
     }
 
-    public void preDecrement(int input){
+    public void preDecrement(int input) {
         System.out.println("Pre decrement: " + --input);
         System.out.println("After pre decrement: " + input);
     }
 
-    public void complementRepresentation(int input){
+    public void complementRepresentation(int input) {
         System.out.println("Complement: " + -input);
         System.out.println("after complement: " + input);
     }
 
-    public void negationOperator(boolean flag){
+    public void negationOperator(boolean flag) {
         System.out.println("negation: " + !flag);
-    }
-
-    public static void main(String[] args) {
-
-        System.out.println("Please enter integer value");
-        Scanner sc = new Scanner(System.in);
-        int inputNumber = sc.nextInt();
-
-        UnaryOperator UnaryOperator = new UnaryOperator();
-
-        UnaryOperator.postIncrement(inputNumber);
-        UnaryOperator.preIncrement(inputNumber);
-        UnaryOperator.postDecrement(inputNumber);
-        UnaryOperator.preDecrement(inputNumber);
-        UnaryOperator.complementRepresentation(inputNumber);
-        UnaryOperator.negationOperator(true);
-
     }
 }
